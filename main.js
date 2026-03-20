00e4e709753c854af353ef2e02a85811c800b18a
KKSTUDIO-dot <sashaparubets2008@gmail.com>
2026-03-20 21:12:00 +0200

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-import { getUserData } from './utils.js';
-
-const { tg } = getUserData();
-tg.expand();
-tg.enableClosingConfirmation();
-
-// Главная страница остается пустой, ничего не делаем
-console.log('Главная страница');
